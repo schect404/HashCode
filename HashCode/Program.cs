b94c8cd715a50481236e0aa122d779ce664f0c69
schect404 <tomilovych@dlit.dp.ua>
2018-03-01 21:15:41 +0200

    True FileReading

--- HashCode/Program.cs
+++ HashCode/Program.cs
@@ -12,12 +12,12 @@ namespace HashCode
         static void Main(string[] args)
         {
             string path = "example.in";
-            string[] text = new string[10000];
-            if (File.Exists(path))
-            {
-                text = File.ReadAllLines(path);
-            }
-
+         
+           
+            
+             string[]   text = File.ReadAllLines(path);
+            
+            List<Rides> rides= new List<Rides>();
             string firstrow = text[0];
             string[] first = firstrow.Split(' ');
             int rows = int.Parse(first[0]);
@@ -27,39 +27,20 @@ namespace HashCode
             int bonus = int.Parse(first[4]);
             int nsteps = int.Parse(first[5]);
             int[,] arr = new int[rows, columns];
-            Time[] times = new Time[rows];
+            
             int j = 0;
             for (int i = 0; i < rows; i++)
             {
-                for (j = 0; j < columns; j++)
-                {
-                    arr[i, j] = int.Parse(text[i + 1].Split(' ')[j]);
-                }
-                times[i].earliest = int.Parse(text[i + 1].Split(' ')[j]);
-                times[i].latest = int.Parse(text[i + 1].Split(' ')[j + 1]);
-            }
-            for (int i = 0; i < rows; i++)
-            {
-                for (int k = 0; k < columns; k++)
-                {
-                    Console.Write(arr[i, k]);
 
-
-                }
-                Console.Write(times[i].earliest);
-                Console.Write(times[i].latest);
-                Console.WriteLine();
+                rides.Add( new Rides(int.Parse(text[i + 1].Split(' ')[0]), int.Parse(text[i + 1].Split(' ')[1]), int.Parse(text[i + 1].Split(' ')[2]), int.Parse(text[i + 1].Split(' ')[3]), int.Parse(text[i + 1].Split(' ')[4]), int.Parse(text[i + 1].Split(' ')[5])));
+  
             }
+           
 
 
         }
 
 
-        struct Time
-        {
-            public int earliest;
-            public int latest;
-        }
     }
 }
 
--- HashCode/rides.cs
+++ HashCode/rides.cs
@@ -8,12 +8,23 @@ namespace HashCode
 {
     class Rides
     {
-        private int[ , ] start_coord;
-        public int[ , ] Start_coord { get; set; }
-        \private int[,] finish_coord;
-        public int[,] Finish_coord { get; set; }
+      
 
+        public Coords Start_coord { get; set; }
+        public Coords Finish_coord { get; set; }
+        public int earliest { get; set; }
+        public int latest { get; set; }
+        public Rides(int x1, int y1, int x2, int y2, int t1, int t2)
+        {
+            Start_coord=new Coords( x1,x2);
+         
+            Finish_coord = new Coords( x2,y2);
+          
+            earliest = t1;
+            latest = t2;
 
+        }
+        
 
     }
 }
